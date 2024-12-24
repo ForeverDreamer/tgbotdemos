d68879f232d4a9447c5f1b83dd720f9ced1c954c
mi-notebook\doer <499361328@qq.com>
2024-12-24 16:41:07 +0700

    批量离开频道功能

--- bot_1/test.py
+++ bot_1/test.py
@@ -99,144 +99,192 @@ async def backup_user_data():
 
 async def restore_user_data():
     """恢复用户的联系人、用户、群组和频道信息"""
-    try:
-        if not os.path.exists(BACKUP_FILE):
-            print("备份文件不存在，无法恢复数据。")
-            return
+    if not os.path.exists(BACKUP_FILE):
+        print("备份文件不存在，无法恢复数据。")
+        return
 
-        print("正在恢复联系人、用户、群组和频道数据...")
-        with open(BACKUP_FILE, "r", encoding="utf-8") as file:
-            backup = json.load(file)
+    print("正在恢复联系人、用户、群组和频道数据...")
+    with open(BACKUP_FILE, "r", encoding="utf-8") as file:
+        backup = json.load(file)
 
-        # # 单向添加联系人
-        # for contact in backup["contacts"]:
-        #     username = contact["username"]
-        #     user_id = contact["id"]
-        #     first_name = contact["first_name"]
-        #     last_name = contact["last_name"]
-        #     try:
-        #         # 如果提供了 username，则通过 username 获取实体
-        #         if username:
-        #             user_entity = await client.get_entity(username)
-        #         elif user_id:
-        #             # 如果提供了 user_id，则直接获取实体
-        #             user_entity = await client.get_entity(user_id)
-        #         else:
-        #             print("需要提供 username 或 user_id 才能添加联系人。")
-        #             return
-        #
-        #         # 确保实体是有效的用户
-        #         if not isinstance(user_entity, types.User):
-        #             print(f"目标 {username or user_id} 不是有效的用户实体，无法添加为联系人。")
-        #             return
-        #
-        #         # 添加联系人，不分享电话号码
-        #         await client(functions.contacts.AddContactRequest(
-        #             id=user_id,
-        #             first_name=first_name or "",  # 获取用户的名字或备用名字
-        #             last_name=last_name or "",  # 获取用户的姓氏或备用姓氏
-        #             phone="",  # 不需要提供电话号码
-        #             add_phone_privacy_exception=False  # 不分享你的电话号码
-        #         ))
-        #         print(
-        #             f"成功添加联系人：{first_name} {last_name} ({username or user_id})")
-        #
-        #     except Exception as e:
-        #         print(f"无法添加联系人 {username or user_id}: {e}")
+    # # 单向添加联系人
+    # for contact in backup["contacts"]:
+    #     username = contact["username"]
+    #     user_id = contact["id"]
+    #     first_name = contact["first_name"]
+    #     last_name = contact["last_name"]
+    #     try:
+    #         # 如果提供了 username，则通过 username 获取实体
+    #         if username:
+    #             user_entity = await client.get_entity(username)
+    #         elif user_id:
+    #             # 如果提供了 user_id，则直接获取实体
+    #             user_entity = await client.get_entity(user_id)
+    #         else:
+    #             print("需要提供 username 或 user_id 才能添加联系人。")
+    #             return
+    #
+    #         # 确保实体是有效的用户
+    #         if not isinstance(user_entity, types.User):
+    #             print(f"目标 {username or user_id} 不是有效的用户实体，无法添加为联系人。")
+    #             return
+    #
+    #         # 添加联系人，不分享电话号码
+    #         await client(functions.contacts.AddContactRequest(
+    #             id=user_id,
+    #             first_name=first_name or "",  # 获取用户的名字或备用名字
+    #             last_name=last_name or "",  # 获取用户的姓氏或备用姓氏
+    #             phone="",  # 不需要提供电话号码
+    #             add_phone_privacy_exception=False  # 不分享你的电话号码
+    #         ))
+    #         print(
+    #             f"成功添加联系人：{first_name} {last_name} ({username or user_id})")
+    #
+    #     except Exception as e:
+    #         print(f"无法添加联系人 {username or user_id}: {e}")
 
-        # # 恢复用户（非直接联系人但有记录）
-        # for user in backup["users"]:
-        #     try:
-        #         # 如果有用户名，尝试通过用户名打开会话
-        #         if user["username"]:
-        #             user_entity = await client.get_entity(user["username"])
-        #             await client.send_message(user_entity, "你好！我是你的新号机器人，已恢复联系。")  # 向用户发送消息
-        #             print(f"已通过用户名恢复用户会话: {user['first_name']} {user['last_name']} ({user['username']})")
-        #         else:
-        #             # 如果没有用户名，尝试通过用户 ID 获取实体
-        #             user_entity = await client.get_entity(user["id"])
-        #             await client.send_message(user_entity, "你好！我是你的新号机器人，已恢复联系。")
-        #             print(f"已恢复用户会话: {user['first_name']} {user['last_name']}")
-        #     except Exception as e:
-        #         print(f"无法恢复用户会话 {user['first_name']} {user['last_name']}: {e}")
-        #
-        # # 恢复群组
-        # for group in backup["groups"]:
-        #     try:
-        #         group_entity = await client.get_entity(group["id"])
-        #         if isinstance(group_entity, types.Chat) or isinstance(group_entity, types.Channel):
-        #             await client(functions.channels.JoinChannelRequest(group_entity))
-        #             print(f"已通过 ID 加入群组: {group['title']}")
-        #         else:
-        #             print(f"群组 {group['title']} 无法加入，因为无法解析实体。")
-        #         # if group["invite_link"]:
-        #         #     # 如果有邀请链接，通过邀请链接加入
-        #         #     invite_hash = group["invite_link"].split("/")[-1]
-        #         #     await client(functions.messages.ImportChatInviteRequest(hash=invite_hash))
-        #         #     print(f"已加入群组: {group['title']}")
-        #         # else:
-        #         #     # 如果没有邀请链接，尝试通过 ID 加入
-        #         #     group_entity = await client.get_entity(group["id"])
-        #         #     if isinstance(group_entity, types.Chat) or isinstance(group_entity, types.Channel):
-        #         #         await client(functions.channels.JoinChannelRequest(group_entity))
-        #         #         print(f"已通过 ID 加入群组: {group['title']}")
-        #         #     else:
-        #         #         print(f"群组 {group['title']} 无法加入，因为无法解析实体。")
-        #     except Exception as e:
-        #         print(f"无法加入群组 {group['title']}: {e}")
+    # # 恢复用户（非直接联系人但有记录）
+    # for user in backup["users"]:
+    #     try:
+    #         # 如果有用户名，尝试通过用户名打开会话
+    #         if user["username"]:
+    #             user_entity = await client.get_entity(user["username"])
+    #             await client.send_message(user_entity, "你好！我是你的新号机器人，已恢复联系。")  # 向用户发送消息
+    #             print(f"已通过用户名恢复用户会话: {user['first_name']} {user['last_name']} ({user['username']})")
+    #         else:
+    #             # 如果没有用户名，尝试通过用户 ID 获取实体
+    #             user_entity = await client.get_entity(user["id"])
+    #             await client.send_message(user_entity, "你好！我是你的新号机器人，已恢复联系。")
+    #             print(f"已恢复用户会话: {user['first_name']} {user['last_name']}")
+    #     except Exception as e:
+    #         print(f"无法恢复用户会话 {user['first_name']} {user['last_name']}: {e}")
+    #
+    # # 恢复群组
+    # for group in backup["groups"]:
+    #     try:
+    #         group_entity = await client.get_entity(group["id"])
+    #         if isinstance(group_entity, types.Chat) or isinstance(group_entity, types.Channel):
+    #             await client(functions.channels.JoinChannelRequest(group_entity))
+    #             print(f"已通过 ID 加入群组: {group['title']}")
+    #         else:
+    #             print(f"群组 {group['title']} 无法加入，因为无法解析实体。")
+    #         # if group["invite_link"]:
+    #         #     # 如果有邀请链接，通过邀请链接加入
+    #         #     invite_hash = group["invite_link"].split("/")[-1]
+    #         #     await client(functions.messages.ImportChatInviteRequest(hash=invite_hash))
+    #         #     print(f"已加入群组: {group['title']}")
+    #         # else:
+    #         #     # 如果没有邀请链接，尝试通过 ID 加入
+    #         #     group_entity = await client.get_entity(group["id"])
+    #         #     if isinstance(group_entity, types.Chat) or isinstance(group_entity, types.Channel):
+    #         #         await client(functions.channels.JoinChannelRequest(group_entity))
+    #         #         print(f"已通过 ID 加入群组: {group['title']}")
+    #         #     else:
+    #         #         print(f"群组 {group['title']} 无法加入，因为无法解析实体。")
+    #     except Exception as e:
+    #         print(f"无法加入群组 {group['title']}: {e}")
 
-        # 恢复频道
-        for channel in backup["channels"]:
-            try:
-                channel_id = channel["username"] or channel["id"]
-                channel_entity = await client.get_entity(channel_id)
-                if isinstance(channel_entity, types.Channel):
-                    await client(functions.channels.JoinChannelRequest(channel_entity))
-                    print(f"已通过 {channel_id} 加入频道: {channel['title']}")
-                    # 设置静音
-                    mute_until = int(time.time()) + 10 * 365 * 24 * 60 * 60  # 静音 10 年
-                    await client(functions.account.UpdateNotifySettingsRequest(
-                        peer=channel_entity,
-                        settings=types.InputPeerNotifySettings(mute_until=mute_until)
-                    ))
-                    # print(f"已静音频道: {channel_entity.title}")
-                else:
-                    print(f"频道 {channel['title']} 无法加入，因为无法解析实体。")
-                # if channel["invite_link"]:
-                #     # 如果有邀请链接，通过邀请链接加入
-                #     invite_hash = channel["invite_link"].split("/")[-1]
-                #     await client(functions.messages.ImportChatInviteRequest(hash=invite_hash))
-                #     print(f"已加入频道: {channel['title']}")
-                # else:
-                #     # 如果没有邀请链接，尝试通过 ID 加入
-                #     channel_entity = await client.get_entity(channel["id"])
-                #     if isinstance(channel_entity, types.Channel):
-                #         await client(functions.channels.JoinChannelRequest(channel_entity))
-                #         print(f"已通过 ID 加入频道: {channel['title']}")
-                #     else:
-                #         print(f"频道 {channel['title']} 无法加入，因为无法解析实体。")
-            except Exception as e:
-                print(f"无法加入频道 {channel['title']}: {e}")
+    # 恢复频道
+    for channel in backup["channels"]:
+        try:
+            channel_id = channel["username"] or channel["id"]
+            channel_entity = await client.get_entity(channel_id)
+            if isinstance(channel_entity, types.Channel):
+                await client(functions.channels.JoinChannelRequest(channel_entity))
+                print(f"已通过 {channel_id} 加入频道: {channel['title']}")
+                # 设置静音
+                mute_until = int(time.time()) + 10 * 365 * 24 * 60 * 60  # 静音 10 年
+                await client(functions.account.UpdateNotifySettingsRequest(
+                    peer=channel_entity,
+                    settings=types.InputPeerNotifySettings(mute_until=mute_until)
+                ))
+                # print(f"已静音频道: {channel_entity.title}")
+            else:
+                print(f"频道 {channel['title']} 无法加入，因为无法解析实体。")
+            # if channel["invite_link"]:
+            #     # 如果有邀请链接，通过邀请链接加入
+            #     invite_hash = channel["invite_link"].split("/")[-1]
+            #     await client(functions.messages.ImportChatInviteRequest(hash=invite_hash))
+            #     print(f"已加入频道: {channel['title']}")
+            # else:
+            #     # 如果没有邀请链接，尝试通过 ID 加入
+            #     channel_entity = await client.get_entity(channel["id"])
+            #     if isinstance(channel_entity, types.Channel):
+            #         await client(functions.channels.JoinChannelRequest(channel_entity))
+            #         print(f"已通过 ID 加入频道: {channel['title']}")
+            #     else:
+            #         print(f"频道 {channel['title']} 无法加入，因为无法解析实体。")
+        except Exception as e:
+            print(f"无法加入频道 {channel['title']}: {e}")
 
-        print("恢复完成！")
-    except Exception as e:
-        print(f"恢复失败: {e}")
+    print("恢复完成！")
+
+
+async def delete_chat_windows():
+    """从聊天列表中删除会话窗口（不删除消息）"""
+    if not os.path.exists(BACKUP_FILE):
+        print("备份文件不存在，无法恢复数据。")
+        return
+
+    print("正在恢复联系人、用户、群组和频道数据...")
+    with open(BACKUP_FILE, "r", encoding="utf-8") as file:
+        backup = json.load(file)
+    user = await client.get_me()
+    for channel in backup["channels"]:
+        channel_id = channel["username"] or channel["id"]
+        try:
+            # 获取聊天实体
+            channel_entity = await client.get_entity(channel_id)
+            # 删除会话窗口
+            await client(functions.messages.DeleteChatUserRequest(
+                chat_id=channel_entity.id,
+                user_id=user  # 当前用户
+            ))
+            print(f"已删除会话窗口: {channel_id}")
+        except Exception as e:
+            print(f"无法删除会话窗口 {channel_id}: {e}")
+
+
+async def leave_channels():
+    """从聊天列表中删除会话窗口（不删除消息）"""
+    if not os.path.exists(BACKUP_FILE):
+        print("备份文件不存在，无法恢复数据。")
+        return
+
+    print("正在恢复联系人、用户、群组和频道数据...")
+    with open(BACKUP_FILE, "r", encoding="utf-8") as file:
+        backup = json.load(file)
+    for channel in backup["channels"]:
+        channel_id = channel["username"] or channel["id"]
+        try:
+            # 获取聊天实体
+            channel_entity = await client.get_entity(channel_id)
+            # 发送离开频道的请求
+            await client(functions.channels.LeaveChannelRequest(channel=channel_entity))
+            print(f"已离开频道: {channel_entity.title}")
+        except Exception as e:
+            print(f"无法离开频道 {channel_id}: {e}")
 
 
 async def main():
     """主函数"""
-    print("欢迎使用 Telegram 备份恢复工具！")
-    print("请选择操作：")
-    print("1. 备份数据")
-    print("2. 恢复数据")
-    choice = input("请输入 1 或 2: ").strip()
+    while True:
+        print("欢迎使用 Telegram 备份恢复工具！")
+        print("请选择操作：")
+        print("1. 备份数据")
+        print("2. 恢复数据")
+        print("3. 离开所有频道")
+        choice = input("请输入 1 或 2 或 3: ").strip()
 
-    if choice == "1":
-        await backup_user_data()
-    elif choice == "2":
-        await restore_user_data()
-    else:
-        print("无效选项，请重试。")
+        if choice == "1":
+            await backup_user_data()
+        elif choice == "2":
+            await restore_user_data()
+        elif choice == "3":
+            await leave_channels()
+        else:
+            print("无效选项，请重试。")
+
 
 if __name__ == "__main__":
     client.start(app_info[-1])  # 启动客户端
